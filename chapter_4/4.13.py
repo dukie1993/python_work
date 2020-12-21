20e8c0b1c7b4c2036d9f7f4f5b42d05a99cbc101
Eugene <dukeunreal@gmail.com>
2020-12-21 21:50:37 +0200

    learning

---
+++ chapter_4/4.13.py
@@ -0,0 +1,7 @@
+sw_table = ('meat', 'bread', 'water', 'salt', 'apple')
+for item in sw_table:
+    print(item.title())
+
+sw_table = ('meat', 'bread', 'water', 'salt', 'apple', 'milk', 'nut')
+for item in sw_table:
+    print(item.title())
---
+++ chapter_5/5.1.py
@@ -0,0 +1,26 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+my_name = 'eugene'
+if my_name == 'eugene':
+    print('My name is really Eugene.')
+
+fav_color = 'blue'
+if fav_color == 'blue':
+    print('My favorite color is - Blue!')
+
+my_wife = 'ira'
+if my_wife == 'ira':
+    print('I am very love her!')
+
+my_age = 27
+if my_age >= 18:
+    print('I can drive a car!')
+
+town = 'lviv'
+if town != 'lutsk':
+    print('Where is your home, brooo?')
